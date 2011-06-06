fb9d680ddaa2585d83a5d2a40fccb978b0db2fad
Jorg Jenni <jorg.jenni@jennius.co.uk>
2011-06-06 11:23:44 +0100

    main method calculate implemented

--- app/teanalyzer.rb
+++ app/teanalyzer.rb
@@ -1,6 +1,9 @@
+require File.expand_path('./../triad', __FILE__)
+
 class Teanalyzer
   def self.calculate word
     triads = Triad.from_word(word)
-
+    efforts = triads.map {|t| t.effort}
+    efforts.reduce(:+) / triads.length unless triads.length == 0
   end
 end
--- spec/teanalyzer_spec.rb
+++ spec/teanalyzer_spec.rb
@@ -1,13 +1,17 @@
 require File.expand_path('./../../app/teanalyzer', __FILE__)
 
 describe 'Teanalyzer' do
-  describe 'calculate' do
-    context 'typing' do
-      it 'should be between 0 and 1' do
-        effort = Teanalyzer.calculate('typing')
-        effort.should >= 0.0
-        effort.should <= 1.0
-      end
-    end
+  subject { Teanalyzer }
+  describe 'compare effort of words' do
+    # some subjective examples using the standard parameter settings
+    specify { subject.calculate('Nerd') > subject.calculate('asdfjkl') }
+    specify { subject.calculate('peeping') > subject.calculate('pedestal') }
+    specify { subject.calculate('scalawag') > subject.calculate('scabbard') }
+    specify { subject.calculate('Velcro') > subject.calculate('yashmak') }
+    specify { subject.calculate('unethical') > subject.calculate('unenviable') }
+    specify { subject.calculate('marquee') > subject.calculate('marooned') }
+    specify { subject.calculate('harry') > subject.calculate('haste') }
+    specify { subject.calculate('yet') > subject.calculate('has') }
+    specify { subject.calculate('dress') > subject.calculate('drench') }
   end
 end
